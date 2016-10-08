d6a555a2370ef30b4621567e581f11d8447d9351
Jake Shasteen <jakeshasteen@gmail.com>
2016-10-08 14:54:15 -0700

    Initialize create-react-app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>HTML Jobs for Modern Nights MUSH</h2>
+        </div>
+        <p className="App-intro">
+          <JobsList>
+          </JobsList>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
